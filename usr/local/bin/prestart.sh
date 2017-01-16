9581c3ab89f85df346a3f94bf5eefb3edb6dc48a
Patrick Sodré <sodre@sodre.co>
2017-01-15 23:35:34 -0500

    Missed guacd.

--- usr/local/bin/prestart.sh
+++ usr/local/bin/prestart.sh
@@ -9,15 +9,25 @@ ln -sf /opt/guacamole/guacamole.war /usr/local/tomcat/webapps/
 
 
 # Refresh the config file
-until [[ `curl -s ${CONSUL}:8500/v1/health/state/passing | grep guacd` ]]
+until [[ `curl -s ${CONSUL}:8500/v1/health/state/passing | grep guacamole-server` ]]
 do
-    echo "guacd is not healthy..."
+    echo "guacamole-server is not healthy..."
     sleep 5
 done
 
+until [[ `curl -s ${CONSUL}:8500/v1/health/state/passing | grep mysql-primary` ]]
+do
+    echo "mysql-primary is not healthy..."
+    sleep 5
+done
 
-# Write the guacd configuration
+# Write the guacamole-client configuration
 consul-template \
     -once \
     -consul localhost:8500 \
     -template "${GUACAMOLE_PROPERTIES}.ctmpl:${GUACAMOLE_PROPERTIES}"
+
+ln -sf /opt/guacamole/mysql/mysql-connector-*.jar "$GUACAMOLE_LIB"
+ln -sf /opt/guacamole/mysql/guacamole-auth-*.jar "$GUACAMOLE_EXT"
+
+
